a849dbed6c94c4189af5a316c0d7657654b6cafd
Akhmedow <akramakhmedoff@gmail.com>
2025-07-04 11:20:05 +0500

    begin

--- work/src/pages/Career/career.jsx
+++ work/src/pages/Career/career.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import logo from './images/LOGO (2).svg'
 const Career = () => {
   return (
-    <div>Akram</div>
+    <div>
+      <div className='bg-[#1D2026] text-white'>
+<div className='border-b'>
+  <img src={logo} alt="" />
+  </div>
+      </div>
+    </div>
   )
 }
 
